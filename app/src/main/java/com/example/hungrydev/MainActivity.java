059e87867064df15e7fac77f08999671f7f0d552
Feihuan Peng <feihuan.peng@spreetail.com>
2025-09-07 01:18:52 -0500

    first commit

---
+++ app/src/main/java/com/example/hungrydev/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hungrydev;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_main);
+    }
+}
