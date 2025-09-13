14029ff556f56a95a5f8f5e8194f4c556f7e1e72
Feihuan Peng <feihuan.peng@spreetail.com>
2025-09-13 18:46:33 -0500

    asd

---
+++ app/src/main/java/com/example/hungrydev/Dish.java
@@ -0,0 +1,21 @@
+package com.example.hungrydev;
+
+import androidx.annotation.NonNull;
+
+public class Dish {
+    String title;
+    String description;
+    int price;
+
+    Dish(String title, String description, int price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+        return title + "\n" + description + "\n" + price;
+    }
+}
